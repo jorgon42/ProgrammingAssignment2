17b5aede4a24a79d9ac4a1c21650e207309a426b
jorg <vodyanoj@gmail.com>
2017-07-07 15:21:54 -0700

    Week 2: cached matrix inverse

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,33 @@
 ## functions do
 
 ## Write a short comment describing this function
+#takes three parameters: matrix(content, row number, column number), and stores them in a list
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()) {
+	m <- NULL
+	set <- function(y=matrix()) {
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(solve) m <<- solve
+	getinverse <- function() m
+	list(set = set,get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
 ## Write a short comment describing this function
+# takes function above with its parameters and returns either a cached value of m (if m is not empty) or calculates
+# the inverse from scratch
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	m <- x$getinverse()
+	if(!is.null(m)) {
+		message("Inverse cached. Getting cached data.")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data, ...)
+	x$setinverse(m)
+	m
 }
